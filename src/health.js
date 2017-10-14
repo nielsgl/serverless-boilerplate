89a743e2c63b05a23d7c9837131302d674164ff7
Niels van Galen Last <nvangalenlast@gmail.com>
2017-10-14 16:38:36 +0200

    update files with editorconfig

--- src/health.js
+++ src/health.js
@@ -1,17 +1,17 @@
 // eslint-disable-next-line import/prefer-default-export
 const health = async (event, context, cb) => {
-  const p = new Promise((resolve) => {
-    resolve('success');
-  });
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      status: 'ok',
-    }),
-  };
-  p
-    .then(() => cb(null, response))
-    .catch(e => cb(e));
+	const p = new Promise((resolve) => {
+		resolve('success');
+	});
+	const response = {
+		statusCode: 200,
+		body: JSON.stringify({
+			status: 'ok',
+		}),
+	};
+	p
+	.then(() => cb(null, response))
+	.catch(e => cb(e));
 };
 
 export default health;
--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,20 @@ const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: slsw.lib.entries,
-  target: 'node',
-  externals: [nodeExternals()],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel-loader'],
-      include: __dirname,
-      exclude: /node_modules/,
-    }],
-  },
-  output: {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
-  },
+	entry: slsw.lib.entries,
+	target: 'node',
+	externals: [nodeExternals()],
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			loaders: ['babel-loader'],
+			include: __dirname,
+			exclude: /node_modules/,
+		}],
+	},
+	output: {
+		libraryTarget: 'commonjs',
+		path: path.join(__dirname, '.webpack'),
+		filename: '[name].js',
+	},
 };
