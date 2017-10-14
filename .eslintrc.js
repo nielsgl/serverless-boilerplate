1c71f166f1a2a102a2f9f9a9efcbb8f290b3434c
Niels van Galen Last <nvangalenlast@gmail.com>
2017-10-14 19:18:52 +0200

    updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,16 @@
 module.exports = {
-    "extends": "airbnb-base",
+	"extends": "airbnb-base",
 	"plugins": [],
 	"rules": {
 		"indent": ["error", "tab", {"MemberExpression": 1}],
 		"no-tabs": "off",
+		"new-cap": ["error", { "newIsCap": false }],
+
+		// doesn't work in node v4 :(
+		// "strict": "off",
+		// "prefer-rest-params": "off",
+		// "react/require-extension" : "off",
+		"import/no-extraneous-dependencies" : "off"
 	},
 	"env": {
 		"es6": true
