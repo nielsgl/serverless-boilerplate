47780157e98dd435ef737f0bc552ad24f719485c
Niels van Galen Last <nvangalenlast@gmail.com>
2017-10-15 18:29:00 +0200

    fix lint errors

--- src/hello.js
+++ src/hello.js
@@ -4,7 +4,12 @@ const error = require('debug')('app:err:src:hello');
 const http = require('../lib/services/http');
 
 const hello = async (event, context, cb) => {
-	http.success({message: 'Hello, Serverless World!', event, context, env: process.env}, cb);
+	http.success({
+		message: 'Hello, Serverless World!',
+		event,
+		context,
+		env: process.env,
+	}, cb);
 };
 
 export default hello;
