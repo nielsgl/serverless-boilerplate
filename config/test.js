2a0b87fde9dd23425d9dc2508cac85c57459af89
Niels van Galen Last <nvangalenlast@gmail.com>
2017-10-17 16:44:28 +0200

    updated scripts

--- config/test.js
+++ config/test.js
@@ -1,3 +1,32 @@
+const service = 'serverless-boilerplate';
+
 module.exports = {
-	env: 'test',
+	service,
+
+	stage: 'test',
+	region: '',
+
+	domain: {
+		basePath: '',
+		domainName: `${service}.foo.com`,
+	},
+
+	app: {
+		host: '0.0.0.0',
+		port: 3000,
+	},
+
+	foo: 'bar',
+
+	env: '',
+
+	db: {
+		local: {
+			region: 'localhost',
+			port: 8000,
+		},
+		tables: {
+			table: `${service}-users`,
+		},
+	},
 };
